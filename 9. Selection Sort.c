3a9b448236ab93a19a9b5bb1661379ed3d86d829
11249A180 <11249A180@kanchiuniv.ac.in>
2025-11-06 11:54:33 +0530

    Update 9. Selection Sort.c

--- 9. Selection Sort.c
+++ 9. Selection Sort.c
@@ -1,31 +1,55 @@
-#include &lt;stdio.h&gt;
-void selectionSort(int arr[], int n) {
-int i, j, minIndex, temp;
-for (i = 0; i &lt; n - 1; i++) {
-minIndex = i;
-for (j = i + 1; j &lt; n; j++) {
-if (arr[j] &lt; arr[minIndex])
-minIndex = j;
-}
-// Swap
-temp = arr[i];
-arr[i] = arr[minIndex];
-arr[minIndex] = temp;
-}
-}
+#include <stdio.h>
+#include <stdlib.h> 
 void printArray(int arr[], int n) {
-for (int i = 0; i &lt; n; i++)
-printf(&quot;%d &quot;, arr[i]);
-printf(&quot;\n&quot;);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
-int main() {
-int arr[] = {64, 25, 12, 22, 11};
-int n = sizeof(arr)/sizeof(arr[0]);
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-printf(&quot;Original array: &quot;);
-printArray(arr, n);
-selectionSort(arr, n);
-printf(&quot;Sorted array (Selection Sort): &quot;);
-printArray(arr, n);
-return 0;
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex;
+
+   for (i = 0; i < n - 1; i++) {
+        // Assume the current element is the minimum
+        minIndex = i;
+
+
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        
+        if (minIndex != i) {
+            swap(&arr[i], &arr[minIndex]);
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+  
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("--- Selection Sort Demonstration ---\n");
+    
+    
+    printf("Original array: ");
+    printArray(arr, n);
+
+  
+    selectionSort(arr, n);
+
+    
+    printf("Sorted array (Selection Sort): ");
+    printArray(arr, n);
+
+    return 0;
 }
